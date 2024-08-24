1f862a91dc476c76208da2de8ca4a39a07226748
Edvard Sire <mail@edvardsire.com>
2024-08-24 15:48:31 +0200

    fmt

--- configuration.nix
+++ configuration.nix
@@ -6,12 +6,11 @@
 
 {
   nixpkgs.config.allowUnfree = true;
-  imports =
-    [
-      ./hardware-configuration.nix
-      ./apple-silicon-support
-      <home-manager/nixos>
-    ];
+  imports = [
+    ./hardware-configuration.nix
+    ./apple-silicon-support
+    <home-manager/nixos>
+  ];
 
   hardware.asahi.peripheralFirmwareDirectory = ./firmware;
   hardware.asahi.useExperimentalGPUDriver = true;
@@ -30,24 +29,10 @@
   virtualisation.docker.enable = true;
   users.extraGroups.docker.members = [ "user" ];
 
-  networking.wg-quick.interfaces = {
-    wg0 = {
-      autostart = true;
-      address = [ "10.0.0.2/24" ];
-      dns = [ "192.168.1.1" ];
-      privateKeyFile = "/home/user/wireguard-keys/private";
+  networking.wg-quick.interfaces.wg0.configFile =
+    "/etc/nixos/files/wireguard/wg0.conf";
 
-      peers = [
-        {
-          publicKey = "Yf4EHYENC7VLrS1xrTuMQ3eGFgcjr4QglMCauj5bcDw=";
-          allowedIPs = [ "0.0.0.0/0" ];
-          endpoint = "pi.edvardsire.com:23";
-          persistentKeepalive = 25;
-        }
-      ];
-    };
-  };
-
+  environment.sessionVariables.NIXOS_OZONE_WL = 1;
   environment.systemPackages = with pkgs; [
     tree
     htop
@@ -69,13 +54,16 @@
     file
     vlc
     jq
+    obsidian
+    eyedropper
+    gnomeExtensions.vitals
+    typer
     #LSP
     ruff-lsp
     clang-tools
     pyright
   ];
 
-  # home-manager.useGlobalPkgs = true;
   users.users.user = {
     isNormalUser = true;
     home = "/home/user";
@@ -83,21 +71,25 @@
   };
   home-manager.useGlobalPkgs = true;
   home-manager.users.user = { pkgs, config, ... }: {
+
     dconf = {
       enable = true;
       settings = {
         "org/gnome/mutter" = {
           experimental-features = [ "scale-monitor-framebuffer" ];
         };
-        "org/gnome/desktop/interface" = {
-          color-scheme = "prefer-dark";
-        };
+        "org/gnome/desktop/interface" = { color-scheme = "prefer-dark"; };
       };
     };
 
-    home.file.".gitconfig".source = config.lib.file.mkOutOfStoreSymlink "/home/user/dotfiles/.gitconfig";
-    home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "/home/user/dotfiles/nvim";
-    home.file.".bashrc".source = config.lib.file.mkOutOfStoreSymlink "/home/user/dotfiles/bash/bashrc";
+    home.sessionVariables.NIXOS_OZONE_WL = "1";
+
+    home.file.".gitconfig".source =
+      config.lib.file.mkOutOfStoreSymlink "/home/user/dotfiles/.gitconfig";
+    home.file.".config/nvim".source =
+      config.lib.file.mkOutOfStoreSymlink "/home/user/dotfiles/nvim";
+    home.file.".bashrc".source =
+      config.lib.file.mkOutOfStoreSymlink "/home/user/dotfiles/bash/bashrc";
 
     home.stateVersion = "24.11"; # Did you read the comment?
   };
@@ -111,47 +103,49 @@
   programs.firefox = {
     enable = true;
     languagePacks = [ "en-US" ];
-     /* ---- POLICIES ---- */
-     # Check about:policies#documentation for options.
+    # ---- POLICIES ----
+    # Check about:policies#documentation for options.
     policies = {
-     DisableTelemetry = true;
-     DisableFirefoxStudies = true;
-     EnableTrackingProtection = {
-       Value= true;
-       Locked = true;
-       Cryptomining = true;
-       Fingerprinting = true;
-     };
-     DisablePocket = true;
-     DisableFirefoxAccounts = true;
-     DisableAccounts = true;
-     DisableFirefoxScreenshots = true;
-     OverrideFirstRunPage = "";
-     OverridePostUpdatePage = "";
-     DontCheckDefaultBrowser = true;
-     DisplayBookmarksToolbar = "never"; # alternatives: "always" or "newtab"
-     DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
-     SearchBar = "unified"; # alternative: "separate"
-     OfferToSaveLogins = false;
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+      DisablePocket = true;
+      DisableFirefoxAccounts = true;
+      DisableAccounts = true;
+      DisableFirefoxScreenshots = true;
+      OverrideFirstRunPage = "";
+      OverridePostUpdatePage = "";
+      DontCheckDefaultBrowser = true;
+      DisplayBookmarksToolbar = "never"; # alternatives: "always" or "newtab"
+      DisplayMenuBar =
+        "default-off"; # alternatives: "always", "never" or "default-on"
+      SearchBar = "unified"; # alternative: "separate"
+      OfferToSaveLogins = false;
 
-     /* ---- EXTENSIONS ---- */
-     # Check about:support for extension/add-on ID strings.
-     # Valid strings for installation_mode are "allowed", "blocked",
-     # "force_installed" and "normal_installed".
-     ExtensionSettings = {
-       # "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
-       "uBlock0@raymondhill.net" = {
-         install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-         installation_mode = "force_installed";
-       };
-       "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-         install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden/latest.xpi";
-         installation_mode = "force_installed";
-       };
-     };
+      # ---- EXTENSIONS ----
+      # Check about:support for extension/add-on ID strings.
+      # Valid strings for installation_mode are "allowed", "blocked",
+      # "force_installed" and "normal_installed".
+      ExtensionSettings = {
+        # "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
+        "uBlock0@raymondhill.net" = {
+          install_url =
+            "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          install_url =
+            "https://addons.mozilla.org/firefox/downloads/latest/bitwarden/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
     };
   };
-  
 
   services.xserver = {
     enable = true;
@@ -159,7 +153,8 @@
     desktopManager.gnome.enable = true;
   };
   services.xserver.xkb.layout = "us";
-  services.xserver.videoDrivers = [ "displaylink" "modesetting" ]; # https://nixos.wiki/wiki/Displaylink
+  services.xserver.videoDrivers =
+    [ "displaylink" "modesetting" ]; # https://nixos.wiki/wiki/Displaylink
 
   environment.gnome.excludePackages = (with pkgs; [
     gnome-photos
@@ -168,7 +163,7 @@
     gnome-console
     gnome-calculator
     gnome-calendar
-    gnome-system-monitor 
+    gnome-system-monitor
     gnome-terminal
     gedit
     epiphany # web browser
@@ -189,10 +184,10 @@
     iagno # go game
     hitori # sudoku game
     atomix # puzzle game
-  ]) ++ (with pkgs; [
-    sushi # file preview
-  ]);
-
+  ]) ++ (with pkgs;
+    [
+      sushi # file preview
+    ]);
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
