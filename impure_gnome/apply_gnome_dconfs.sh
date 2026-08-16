d74fb79080c9eb908a813340178e0930600a7f4a
Edvard Sire <edvard.sire@sixrobotics.com>
2026-08-16 20:06:42 +0200

    gnome: update impure dconf

--- impure_gnome/apply_gnome_dconfs.sh
+++ impure_gnome/apply_gnome_dconfs.sh
@@ -20,16 +20,32 @@
 ### reboot
 ### gnome-extensions enable launch-new-instance@gnome-shell-extensions.gcampax.github.com
 
+### previous settings
+# dconf write /org/gnome/mutter/workspaces-only-on-primary true
+# dconf write /org/gnome/desktop/remote-desktop/rdp/screen-share-mode "'extend'"
+#
+major=$(
+    gdbus call --session \
+        --dest org.gnome.Shell \
+        --object-path /org/gnome/Shell \
+        --method org.freedesktop.DBus.Properties.Get \
+        org.gnome.Shell ShellVersion |
+    grep -oE '[0-9]+' |
+    head -n1
+)
+
+[ "${major:-0}" -ge 50 ] || {
+    echo "GNOME Shell >= 50 required" >&2
+    exit 1
+}
+
 dconf write /org/gnome/mutter/experimental-features "['scale-monitor-framebuffer']"
 dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
-dconf write /org/gnome/desktop/remote-desktop/rdp/screen-share-mode "'extend'"
 dconf write /org/gnome/desktop/interface/show-battery-percentage true
-dconf write /org/gnome/mutter/workspaces-only-on-primary true
 dconf write /org/gnome/desktop/interface/clock-show-seconds true
 
 dconf write /org/gnome/desktop/calendar/show-weekdate true
-#https://github.com/F-i-f/weeks-start-on-monday
-
+dconf write /org/gnome/desktop/calendar/week-start-day "'monday'"
 
 # autohide dock/sidebar
 dconf write /org/gnome/shell/extensions/dash-to-dock/dock-fixed false
